5f7bf9d5c2f79d8529f857f47a4ed9bfba48ea20
Evgenij <orkinevgenij@gmail.com>
2026-05-10 15:03:19 +0300

    Update data-source.ts

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,16 +1,17 @@
 import { config } from 'dotenv'
 import { DataSource } from 'typeorm'
-
 config()
 
 export default new DataSource({
   type: 'postgres',
-  url: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   entities: ['dist/**/*.entity{.ts,.js}'],
-  migrations: ['dist/migrations/*.js'],
-  subscribers: ['dist/subscriber/*.js'],
+  migrations: ['src/migrations/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  dropSchema: false,
   synchronize: false,
 })
